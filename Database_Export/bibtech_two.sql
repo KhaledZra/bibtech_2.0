2ce2f96d252e1ca297a5e998b6b7016e69c56931
Khaled Zra <halo_434000@hotmail.com>
2022-12-08 00:16:11 +0100

    updated DB export

--- Database_Export/bibtech_two.sql
+++ Database_Export/bibtech_two.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2022 at 03:50 PM
+-- Generation Time: Dec 08, 2022 at 12:12 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,33 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `bibtech_two`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `account`
---
-
-CREATE TABLE `account` (
-  `id` int(11) NOT NULL,
-  `user_name` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `first_name` varchar(32) NOT NULL,
-  `last_name` varchar(32) NOT NULL,
-  `email` varchar(32) NOT NULL,
-  `phone_number` varchar(32) NOT NULL,
-  `permission_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `account`
---
-
-INSERT INTO `account` (`id`, `user_name`, `password`, `first_name`, `last_name`, `email`, `phone_number`, `permission_id`) VALUES
-(1, 'kalle', 'pw123', 'Khaled', ' Zraiqi', 'khaled@gmail.com', '0736972075', 1),
-(2, 'shah', 'pw123', 'Shahin', ' Zraiqi', 'shahin@gmail.com', '0724572122', 2),
-(3, 'rally', 'pw123', 'Tariq', ' Zraiqi', 'tariq@gmail.com', '0711111111', 2),
-(4, 'konstans', 'pw123', 'Tinos', ' Zraiqi', 'tinos@gmail.com', '0722222222', 2);
 
 -- --------------------------------------------------------
 
@@ -103,25 +76,50 @@ INSERT INTO `author_to_book` (`author_id`, `book_id`) VALUES
 CREATE TABLE `book` (
   `id` int(11) NOT NULL,
   `title` varchar(32) NOT NULL,
+  `is_available` tinyint(1) NOT NULL,
   `media_id` int(11) NOT NULL,
-  `library_id` int(11) DEFAULT NULL,
-  `available` tinyint(4) NOT NULL
+  `library_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`id`, `title`, `media_id`, `library_id`, `available`) VALUES
+INSERT INTO `book` (`id`, `title`, `is_available`, `media_id`, `library_id`) VALUES
 (1, 'Harry Potter', 1, 1, 1),
-(2, 'Harry Potter', 2, 1, 1),
-(3, 'Harry Potter', 3, 1, 1),
-(4, 'Song of Ice and Fire', 1, 2, 1),
-(5, 'Song of Ice and Fire', 2, 2, 1),
-(6, 'Song of Ice and Fire', 3, 2, 1),
-(7, 'The Lord of the Rings', 1, 3, 1),
-(8, 'The Lord of the Rings', 2, 3, 1),
-(9, 'The Lord of the Rings', 3, 3, 1);
+(2, 'Harry Potter', 1, 2, 1),
+(3, 'Harry Potter', 1, 3, 1),
+(4, 'Song of Ice and Fire', 1, 1, 2),
+(5, 'Song of Ice and Fire', 1, 2, 2),
+(6, 'Song of Ice and Fire', 1, 3, 2),
+(7, 'The Lord of the Rings', 1, 1, 3),
+(8, 'The Lord of the Rings', 1, 2, 3),
+(9, 'The Lord of the Rings', 1, 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `pin_code` varchar(32) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `phone_number` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `pin_code`, `first_name`, `last_name`, `email`, `phone_number`) VALUES
+(1, '1234', 'Khaled', ' Zraiqi', 'khaled@gmail.com', '0700000000'),
+(2, '1234', 'Shahin', ' Zraiqi', 'shahin@gmail.com', '0724572122'),
+(3, '1234', 'Tariq', ' Zraiqi', 'tariq@gmail.com', '0711111111'),
+(4, '1234', 'Tinos', ' Zraiqi', 'tinos@gmail.com', '0722222222');
 
 -- --------------------------------------------------------
 
@@ -132,15 +130,20 @@ INSERT INTO `book` (`id`, `title`, `media_id`, `library_id`, `available`) VALUES
 CREATE TABLE `librarian` (
   `id` int(11) NOT NULL,
   `library_id` int(11) NOT NULL,
-  `account_id` int(11) NOT NULL
+  `user_name` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `phone_number` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `librarian`
 --
 
-INSERT INTO `librarian` (`id`, `library_id`, `account_id`) VALUES
-(1, 1, 1);
+INSERT INTO `librarian` (`id`, `library_id`, `user_name`, `password`, `first_name`, `last_name`, `email`, `phone_number`) VALUES
+(1, 1, 'kalle', '1234', 'Khaled', ' Zraiqi', 'khaled@gmail.com', '0700000000');
 
 -- --------------------------------------------------------
 
@@ -174,8 +177,9 @@ CREATE TABLE `loan` (
   `id` int(11) NOT NULL,
   `start_date` date NOT NULL,
   `due_date` date DEFAULT NULL,
+  `is_returned` tinyint(1) NOT NULL,
   `book_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `customer_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -198,35 +202,9 @@ INSERT INTO `media` (`id`, `name`) VALUES
 (2, 'Audio book'),
 (3, 'E-book');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `permission`
---
-
-CREATE TABLE `permission` (
-  `id` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `permission`
---
-
-INSERT INTO `permission` (`id`, `name`) VALUES
-(1, 'Admin'),
-(2, 'User');
-
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `permission_id` (`permission_id`);
 
 --
 -- Indexes for table `author`
@@ -247,15 +225,20 @@ ALTER TABLE `author_to_book`
 ALTER TABLE `book`
   ADD PRIMARY KEY (`id`),
   ADD KEY `media_id` (`media_id`),
-  ADD KEY `foreign_key_name` (`library_id`);
+  ADD KEY `library_id` (`library_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `librarian`
 --
 ALTER TABLE `librarian`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `library_id` (`library_id`),
-  ADD KEY `account_id` (`account_id`);
+  ADD KEY `library_id` (`library_id`);
 
 --
 -- Indexes for table `library`
@@ -269,7 +252,7 @@ ALTER TABLE `library`
 ALTER TABLE `loan`
   ADD PRIMARY KEY (`id`),
   ADD KEY `book_id` (`book_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `customer_id` (`customer_id`);
 
 --
 -- Indexes for table `media`
@@ -278,20 +261,8 @@ ALTER TABLE `media`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `permission`
---
-ALTER TABLE `permission`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `author`
@@ -304,6 +275,12 @@ ALTER TABLE `author`
 --
 ALTER TABLE `book`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `librarian`
@@ -330,20 +307,8 @@ ALTER TABLE `media`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `permission`
---
-ALTER TABLE `permission`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `account`
---
-ALTER TABLE `account`
-  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`);
 
 --
 -- Constraints for table `author_to_book`
@@ -363,15 +328,14 @@ ALTER TABLE `book`
 -- Constraints for table `librarian`
 --
 ALTER TABLE `librarian`
-  ADD CONSTRAINT `librarian_ibfk_1` FOREIGN KEY (`library_id`) REFERENCES `library` (`id`),
-  ADD CONSTRAINT `librarian_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+  ADD CONSTRAINT `librarian_ibfk_1` FOREIGN KEY (`library_id`) REFERENCES `library` (`id`);
 
 --
 -- Constraints for table `loan`
 --
 ALTER TABLE `loan`
   ADD CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `account` (`id`);
+  ADD CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
